4e824efffef0dc022b9aa8c8ee43f3fd2fc385cf
mohammed sufiyan <mohdsufiyan3382@gmail.com>
2026-09-08 10:21:43 +0530

    Add files via upload

---
+++ @23.py
@@ -0,0 +1,19 @@
+hour = int(input("Enter hour (0-23): "))
+
+if 5 <= hour < 12:
+    print("Morning")
+elif 12 <= hour < 17:
+    print("Afternoon")
+elif 17 <= hour < 21:
+    print("Evening")
+else:
+    print("Night")
+
+
+
+
+
+
+
+
+
