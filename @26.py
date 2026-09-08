2234812f2c8ee0beab24bf3fe9da840916743867
mohammed sufiyan <mohdsufiyan3382@gmail.com>
2026-09-08 10:26:13 +0530

    Add files via upload

---
+++ @26.py
@@ -0,0 +1,25 @@
+n = int(input("Enter number (1-7): "))
+
+days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+]
+
+if 1 <= n <= 7:
+    print(days[n - 1])
+else:
+    print("Invalid number")
+
+
+
+
+
+
+
+
+
